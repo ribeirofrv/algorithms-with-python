94463101f59de43e49f9c03ff9423bc3a6181e3c
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-26 14:57:37 -0300

    feat: challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,24 @@
+# def study_schedule(permanence_period, target_time):
+#     if type(target_time) is not int:
+#         return None
+
+#     count = 0
+#     for start_time, end_time in permanence_period:
+#         if type(start_time) is not int or type(end_time) is not int:
+#             return None
+#         elif start_time <= target_time <= end_time:
+#             count += 1
+#     return count
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    counts = {}
+    for start_time, end_time in permanence_period:
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+        for i in range(start_time, end_time + 1):
+            counts[i] = counts.get(i, 0) + 1
+
+    return counts.get(target_time, 0)
