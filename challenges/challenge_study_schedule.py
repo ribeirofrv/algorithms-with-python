0bb7b884fcd837315e8d45ac3136831d680c6505
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-27 19:59:42 -0300

    fix: challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,24 +1,24 @@
-# def study_schedule(permanence_period, target_time):
-#     if type(target_time) is not int:
-#         return None
-
-#     count = 0
-#     for start_time, end_time in permanence_period:
-#         if type(start_time) is not int or type(end_time) is not int:
-#             return None
-#         elif start_time <= target_time <= end_time:
-#             count += 1
-#     return count
-
 def study_schedule(permanence_period, target_time):
-    if not isinstance(target_time, int):
+    if type(target_time) is not int:
         return None
 
-    counts = {}
+    count = 0
     for start_time, end_time in permanence_period:
-        if not isinstance(start_time, int) or not isinstance(end_time, int):
+        if type(start_time) is not int or type(end_time) is not int:
             return None
-        for i in range(start_time, end_time + 1):
-            counts[i] = counts.get(i, 0) + 1
+        elif start_time <= target_time <= end_time:
+            count += 1
+    return count
 
-    return counts.get(target_time, 0)
+# def study_schedule(permanence_period, target_time):
+#     if not isinstance(target_time, int):
+#         return None
+
+#     counts = {}
+#     for start_time, end_time in permanence_period:
+#         if not isinstance(start_time, int) or not isinstance(end_time, int):
+#             return None
+#         for i in range(start_time, end_time + 1):
+#             counts[i] = counts.get(i, 0) + 1
+
+#     return counts.get(target_time, 0)
