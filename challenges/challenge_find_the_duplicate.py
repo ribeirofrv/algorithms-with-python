9cd77bebd3e90796e3011aafc96147655b37d2f0
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-27 22:50:09 -0300

    feat: challenge find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i+1]:
+            return nums[i]
+
+    return False
