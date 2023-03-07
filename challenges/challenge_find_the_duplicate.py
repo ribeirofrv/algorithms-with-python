4f7441a7e58a6e316e1e62902f13bfb38fbd3513
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-07 03:20:11 -0300

    fix: find_duplicate is too complex

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i+1]:
+            return nums[i]
+
+    return False
