81c7cce492c68d69091d54e0304da5ebe88b8b63
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-07 03:09:43 -0300

    feat: challenge anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,38 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_anagram(first_str, second_str):
+    if not first_str and not second_str:
+        return (first_str, second_str, False)
+
+    first_sorted = merge_sort(first_str.lower())
+    second_sorted = merge_sort(second_str.lower())
+    equality = first_sorted == second_sorted
+
+    return (first_sorted, second_sorted, equality)
+
+
+def merge_sort(str):
+    if len(str) <= 1:
+        return str
+
+    middle = len(str) // 2
+    left_half = merge_sort(str[:middle])
+    right_half = merge_sort(str[middle:])
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return "".join(result)
