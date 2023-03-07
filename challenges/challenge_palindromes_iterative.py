bd776a0e7c6a349dee66749b007eaf9ed328ee99
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-07 03:42:52 -0300

    feat: challenge palindrome iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    new_word = ""
+    index = -1
+
+    while len(word) != len(new_word):
+        new_word += word[index]
+        index -= 1
+
+    return True if word == new_word else False
