081ecbd21dcd45ef2eeef6d72590b541860e823c
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-07 03:11:36 -0300

    zerr

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,15 +1,3 @@
 def find_duplicate(nums):
-    if not nums or len(nums) < 2:
-        return False
-
-    for num in nums:
-        if not isinstance(num, int) or num < 1:
-            return False
-
-    nums.sort()
-
-    for i in range(len(nums) - 1):
-        if nums[i] == nums[i+1]:
-            return nums[i]
-
-    return False
+    """Faça o código aqui."""
+    raise NotImplementedError
